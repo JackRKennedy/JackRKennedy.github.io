b3bee593a2da0fff38e65fbb0b3604888700af42
Jack Kennedy <k.jack6@gmail.com>
2024-05-25 21:19:22 +1000

    Refactor website layout with separate HTML pages
    The website structure has been overhauled to utilize individual HTML pages for various content sections. Primarily, this change introduced new pages for 'About', 'Projects', 'CV', and 'Data Visualisations', and updated the contents correspondingly. Additionally, the main navigation bar was updated to properly link to these newly created individual pages. Further, scripts.js was amended to include PDF display functionality for the CV page.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,3 @@
-// Add your JavaScript for animations here
 document.addEventListener('DOMContentLoaded', function () {
     // Example animation: Smooth scrolling for navigation
     const links = document.querySelectorAll('nav ul li a');
@@ -13,4 +12,31 @@ document.addEventListener('DOMContentLoaded', function () {
             });
         });
     }
+
+    // Add PDF display
+    // Specify the URL of the PDF file
+    var url = 'Jack_Kennedy_CV_May_2024.pdf';
+
+    // Create PDFJS document load task
+    var loadingTask = pdfjsLib.getDocument(url);
+    loadingTask.promise.then(function (pdf) {
+        // Fetch the first page
+        pdf.getPage(1).then(function (page) {
+            var scale = 1.5;
+            var viewport = page.getViewport({ scale: scale });
+
+            // Prepare canvas using PDF page dimensions
+            var canvas = document.getElementById('the-canvas');
+            var context = canvas.getContext('2d');
+            canvas.height = viewport.height;
+            canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+            };
+            page.render(renderContext);
+        });
+    });
 });
